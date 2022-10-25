44756058dd41a10aa8dd46b8282ab361302648eb
CalebHouse99 <calebhouse99@gmail.com>
2022-10-25 10:57:01 -0700

    task 5 init commit

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,12 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls == None:
+            return FileStorage.__objects
+        else:
+            return cls.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -47,4 +50,10 @@ class FileStorage:
                 for key, val in temp.items():
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
+            pass
+
+    def delete(self, obj=None):
+        if obj in __objects:
+            del obj
+        else:
             pass
