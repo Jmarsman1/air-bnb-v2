5591abae2dec77c8914f5073d862adfb857c6f89
CalebHouse99 <calebhouse99@gmail.com>
2022-10-27 11:44:14 -0700

    mostly fixed imports

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
-import SQLAlchemy
-from file_storage import FileStorage
+import sqlalchemy
+from models.engine.file_storage import FileStorage
+from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 
 Base = declarative_base()
