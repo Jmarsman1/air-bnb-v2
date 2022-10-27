0c11600747e7218a173e19bdd9a63b47739fd227
CalebHouse99 <calebhouse99@gmail.com>
2022-10-27 12:18:01 -0700

    city and state creation works

--- models/state.py
+++ models/state.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer
+import sqlalchemy
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    id = sqlalchemy.orm.column_property(Column(Integer, primary_key=True, nullable=False))
     
